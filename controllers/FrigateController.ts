6d6078e6e345805210659b5518bb587be962cdd0
Wouter de Bruijn <wouter@hedium.nl>
2023-11-09 21:09:14 +0100

    🥅 Added error alert

--- controllers/FrigateController.ts
+++ controllers/FrigateController.ts
@@ -1,3 +1,4 @@
+import { Alert } from "react-native";
 import { Camera } from "./SettingsController";
 
 interface FrigateCamera {
@@ -20,19 +21,25 @@ export class FrigateController {
   }
 
   async getCameras(): Promise<Camera[]> {
-    const response = await fetch(`${this.host}/api/config`);
-    const config = await response.json() as Config;
-
-    const cameras = Object.keys(config.cameras).map((key) => {
-        const camera = config.cameras[key];
-        return {
-            name: key,
-            enabled: false,
-            unavailable: false,
-            stream: camera.live.stream_name
-        }
-    });
-
-    return cameras;
+    try {
+      const response = await fetch(`${this.host}/api/config`);
+      const config = await response.json() as Config;
+  
+      const cameras = Object.keys(config.cameras).map((key) => {
+          const camera = config.cameras[key];
+          return {
+              name: key,
+              enabled: false,
+              unavailable: false,
+              stream: `${this.host}/live/webrtc/api/stream.mp4?src=${camera.live.stream_name}&video=h264`,
+              thumbnail: `${this.host}/api/${key}/latest.jpg?h=200`,
+          }
+      });
+  
+      return cameras;
+    } catch (e) {
+      Alert.alert("Error", "Unable to fetch cameras from Frigate");
+      return [];
+    }
   }
 }
