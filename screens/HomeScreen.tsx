39835260fe11a3736f53d77e19538f21f8031efd
Wouter de Bruijn <wouter@hedium.nl>
2023-11-09 21:07:07 +0100

    🎨 Cleaned up homepage

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -2,21 +2,13 @@ import * as React from "react";
 import { View, Text, Settings, FlatList } from "react-native";
 import Player from "../components/Player";
 import * as ScreenOrientation from 'expo-screen-orientation';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import { SettingController } from "../controllers/SettingsController";
 
 
 export default function HomeScreen({ navigation }: any) {
-  async function getSettings() {
-    try {
-      const jsonValue = await AsyncStorage.getItem('settings')
-      return jsonValue != null ? JSON.parse(jsonValue) : undefined;
-    } catch (e) {
-      // error reading value
-    }
-  }
-
   const [host, setHost] = React.useState<string>("");
   const [cameras, setCameras] = React.useState<any[]>([]);
+  const settingsController = new SettingController();
 
   function onVideoClick(uri: string) {
     navigation.navigate("VideoDetail", { uri });
@@ -26,33 +18,29 @@ export default function HomeScreen({ navigation }: any) {
     const unsubscribe = navigation.addListener('focus', () => {
       ScreenOrientation.unlockAsync();
 
-      getSettings().then(settings => {
-        if (settings) {
-          setHost(settings.host);
-          setCameras(settings.cameras.filter((camera: any) => camera.enabled));
-        }
+      settingsController.getHost().then((host) => {
+        setHost(host);
       });
-
+      settingsController.getCameras().then((cameras) => {
+        setCameras(cameras);
+      });
     });
-
-    return unsubscribe;
-  });
+  }, []);
 
   function CameraView() {
     return (
-      <View style={{ flex: 1, gap: 5 }}>
-        <FlatList
-          data={cameras}
-          renderItem={({ item }) => <Player uri={`${host}/live/webrtc/api/stream.mp4?src=${item.stream}&video=h264`} onClick={onVideoClick} />}
-        />
-      </View>
+      <FlatList
+        data={cameras.filter((c: any) => c.enabled)}
+        renderItem={({ item }) => <Player uri={item.stream} onClick={onVideoClick} />}
+        keyExtractor={item => item.name}
+      />
     )
   }
 
   return (
     host !== "" || cameras.length === 0 ?
 
-      <View style={{ flex: 1, padding: 16, gap: 5 }}>
+      <View style={{margin: 5}}>
         <CameraView />
       </View>
 
